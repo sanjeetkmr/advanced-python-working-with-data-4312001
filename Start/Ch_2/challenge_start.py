5184d06f27f5d7068a67a4d37b6ed594214d1a1d
Sanjeet Kumar <sanjeet1.kumar@intel.com>
2024-02-16 08:53:48 +0000

    Chap2 practice collections

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,8 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+import os.path
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open(os.path.dirname(__file__) +'/'+"../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
